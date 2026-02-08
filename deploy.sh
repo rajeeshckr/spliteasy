d840081e7c0c6c800996a504644f860b0dbe81a2
rajeeshckr <rajeesh.ckr@gmail.com>
2026-02-08 18:15:16 +1100

    Add deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo podman build -t localhost/spliteasy-backend:latest ~/spliteasy/backend
+
+sudo podman build \
+    --build-arg VITE_API_URL=http://192.168.1.30:9090 \
+    -t localhost/spliteasy-webclient:latest \
+    ~/spliteasy/web-client
+
+sudo systemctl restart podman-spliteasy-backend.service
+sudo systemctl restart podman-spliteasy-webclient.service
